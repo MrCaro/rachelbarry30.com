752a4d1960574b1199a70fbffc18cbcdee7d0d4a
MrCaro <36524539+MrCaro@users.noreply.github.com>
2026-02-19 15:29:49 -0500

    fix: text size mobile

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -2,13 +2,13 @@ export default function Banner() {
   return (
     <div className="pointer-events-none absolute inset-x-0 bottom-0 z-1 px-6 pb-6">
       <div className="pointer-events-auto mx-auto w-fit rounded-xl p-6 text-center">
-        <p className="font-allura text-2xl font-bold text-[#C82A5A] md:text-4xl">
+        <p className="font-allura text-3xl font-bold text-[#C82A5A] md:text-4xl">
           Rachel's Tequila Sunrise Birthday Party
         </p>
         <p className="mt-3 text-xs font-bold text-slate-800 md:text-sm">
           1010 Persimmon St, Kissimmee, FL 34747
         </p>
-        <p className="text-xs font-bold text-slate-800 md:text-sm">
+        <p className="mt-2 text-xs font-bold text-slate-800 md:text-sm">
           April 4th, 2026 at 12:30 PM
         </p>
       </div>
