298419b574ba824835ff74d55d46746153c642fa
MrCaro <36524539+MrCaro@users.noreply.github.com>
2026-02-19 15:24:18 -0500

    fix: text size mobile

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -1,17 +1,17 @@
 export default function Banner() {
-	return (
-		<div className="absolute z-1 pointer-events-none inset-x-0 bottom-0 px-6 pb-6">
-			<div className="pointer-events-auto mx-auto w-fit rounded-xl p-6 text-center">
-				<p className="font-allura text-4xl text-[#C82A5A] font-bold">
-					Rachel's Tequila Sunrise Birthday Party
-				</p>
-				<p className="text-sm/6 text-slate-800 font-bold mt-3">
-					1010 Persimmon St, Kissimmee, FL 34747
-				</p>
-				<p className="text-sm/6 text-slate-800 font-bold">
-					April 4th, 2026 at 12:30 PM
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="pointer-events-none absolute inset-x-0 bottom-0 z-1 px-6 pb-6">
+      <div className="pointer-events-auto mx-auto w-fit rounded-xl p-6 text-center">
+        <p className="font-allura text-2xl font-bold text-[#C82A5A] md:text-4xl">
+          Rachel's Tequila Sunrise Birthday Party
+        </p>
+        <p className="mt-3 text-sm/6 font-bold text-slate-800">
+          1010 Persimmon St, Kissimmee, FL 34747
+        </p>
+        <p className="text-sm/6 font-bold text-slate-800">
+          April 4th, 2026 at 12:30 PM
+        </p>
+      </div>
+    </div>
+  );
 }
